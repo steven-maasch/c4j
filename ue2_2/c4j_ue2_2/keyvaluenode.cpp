26a4e3ded8483170710dfcdac9a72f4100758d9f
Steven Maasch <maasch@steven-maasch.de>
2013-11-14 17:38:10 +0100

    Keine Ahnung.

--- ue2_2/c4j_ue2_2/keyvaluenode.cpp
+++ ue2_2/c4j_ue2_2/keyvaluenode.cpp
@@ -6,24 +6,36 @@ namespace rn {
 namespace internal {
 
             KeyValueNode::KeyValueNode(const key_type& key, const mapped_type& value)
-                : m_key(key), m_value(value)
+                : m_key(key), m_value(value), m_left(0), m_right(0)
             {
             }
 
             KeyValueNode::~KeyValueNode()
             {
-
+                delete m_left;
+                delete m_right;
             }
 
             KeyValueNode* KeyValueNode::find(const key_type& key) {
                 std::cout << "find" << std::endl;
-                if(this->m_key == key) {
+                if(m_key == key) {
                     return this;
                 }
+
+                if(key < m_key) {
+                    return m_left->find(key);
+                }
+
+                if(m_key < key) {
+                    return m_right->find(key);
+                }
+
                 return 0;
             }
 
             void KeyValueNode::insert(const key_type& key, const mapped_type& type) {
+
+
 
             }
 
