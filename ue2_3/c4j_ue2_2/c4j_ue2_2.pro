1154ef93ec4751b82cd46c5ba98442026f81d2d3
Steven Maasch <maasch@steven-maasch.de>
2013-12-04 13:58:55 +0100

    Aufgabe 2.2 fertig.

---
+++ ue2_3/c4j_ue2_2/c4j_ue2_2.pro
@@ -0,0 +1,16 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main_a22.cpp \
+    rationalnumber.cpp \
+    map.cpp \
+    keyvaluenode.cpp
+
+HEADERS += \
+    rationalnumber.h \
+    keyvaluenode.h \
+    map.h \
+    _map.h
+
---
+++ ue2_3/c4j_ue2_2/map.cpp
@@ -0,0 +1,74 @@
+#include "map.h"
+#include "rationalnumber.h"
+#include <cstring>
+#include <iostream>
+#include "keyvaluenode.h"
+
+using namespace std;
+using namespace rn::internal;
+
+namespace rn {
+
+    Map::Map()
+        : root(0)
+    {
+        cout << "Invoking map default constructor." << endl;
+    }
+
+    Map::Map(const Map& rhs)
+        : root(0)
+    {
+       *this = rhs;
+       cout << "Invoking map copy constructor." << endl;
+    }
+
+    Map::~Map()
+    {
+        if(root) {
+            delete root;
+        }
+        cout << "Destroying map" << endl;
+    }
+
+    Map& Map::operator=(const Map& rhs) {
+
+
+        if (this == &rhs) {
+            return *this;
+        }
+
+        if(root) {
+            delete root;
+        }
+
+        root = rhs.root->clone();
+
+        return *this;
+    }
+
+    bool Map::contains(const Map::key_type& key) const {
+
+        if(root == 0) {
+            return false;
+        } else {
+            if(root->find(key)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    Map::mapped_type& Map::operator [](const key_type& other) {
+
+        if(root == 0) {
+            root = new KeyValueNode(other, 0);
+            return root->find(other)->value();
+        } else {
+            if(!contains(other)) {
+                 root->insert(other, 0);
+            }
+        }
+        return root->find(other)->value();
+    }
+}
